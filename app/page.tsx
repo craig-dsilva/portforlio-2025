7dc483d31062e3d1f8ce0d3aa2b24fc619f2c35f
Craig D'Silva <craig.dsilva10@gmail.com>
2025-11-03 12:34:03 +0000

    Add text on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <h2>I am Craig D&apos;Silva and I am a Software Engineer based in London</h2>
+    </>
+  );
 };
 
 export default Home;
